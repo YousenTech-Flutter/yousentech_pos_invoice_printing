2f061abc1e6680afd30778522fe0749f64acc398
eemzain <hudazain55@gmail.com>
2025-08-12 14:58:18 +0300

    .

--- lib/print_invoice/utils/test.dart
+++ lib/print_invoice/utils/test.dart
@@ -3,61 +3,39 @@ import 'dart:ui';
 
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
 import 'package:pos_shared_preferences/pos_shared_preferences.dart';
+import 'package:screenshot/screenshot.dart';
 import 'package:ysn_pos_android_printer/android_printer/printer.dart';
 
 class ScreenshotWidget extends StatefulWidget {
   final Widget child;
   final String? printerIp;
-  const ScreenshotWidget({required this.child,required this.printerIp,  Key? key}) : super(key: key);
+  const ScreenshotWidget(
+      {required this.child, required this.printerIp, Key? key})
+      : super(key: key);
 
   @override
   State<ScreenshotWidget> createState() => _ScreenshotWidgetState();
 }
 
 class _ScreenshotWidgetState extends State<ScreenshotWidget> {
-  final GlobalKey _repaintKey = GlobalKey();
 
-  Future<Uint8List?> capturePng() async {
-    try {
-      RenderRepaintBoundary? boundary =
-          _repaintKey.currentContext?.findRenderObject() as RenderRepaintBoundary?;
-      if (boundary == null) {
-        print("repaintBoundary is null");
-        return null;
-      }
-
-      if (boundary.debugNeedsPaint) {
-        print("Boundary still needs paint!");
-        await Future.delayed(const Duration(milliseconds: 20));
-        return capturePng(); // حاول مجدد بعد تأخير بسيط
-      }
-
-      final image = await boundary.toImage(pixelRatio: 3);
-      ByteData? byteData = await image.toByteData(format: ImageByteFormat.png);
-      return byteData?.buffer.asUint8List();
-    } catch (e) {
-      print("Error capturing image: $e");
-      return null;
-    }
-  }
-
+  ScreenshotController screenshotController = ScreenshotController();
   @override
   void initState() {
     super.initState();
     print("===============initState");
     WidgetsBinding.instance.addPostFrameCallback((_) async {
-       print("===============WidgetsBinding.instance");
-      Uint8List? imageBytes = await capturePng();
-      if (imageBytes != null) {
-        // هنا تستخدم الصورة حسب حاجتك
-        print("Image captured, length = ${imageBytes.length}");
-        testPrint(imageThatC: imageBytes, printerIp: widget.printerIp);
-      } else {
-        print("Failed to capture image.");
-      }
-      Get.back();
+      print("WidgetsBinding.instance.addPostFrameCallback ===============");
+      screenshotController.capture(
+        delay: const Duration(milliseconds: 10),
+      ).then((image) async {
+        testPrint(imageThatC: image!);
+        Get.back();
+      }).catchError((onError) {
+      });
     });
   }
 
@@ -66,12 +44,9 @@ class _ScreenshotWidgetState extends State<ScreenshotWidget> {
     print("===============Widget build");
     return Offstage(
       offstage: true,
-      child: RepaintBoundary(
-        key: _repaintKey,
-        child: Directionality(
-          textDirection: SharedPr.lang == "ar" ? TextDirection.rtl : TextDirection.ltr,
-          child: widget.child,
-        ),
+      child: Screenshot(
+        controller:screenshotController,
+        child: SizedBox(width: 150.w, child: widget.child),
       ),
     );
   }
