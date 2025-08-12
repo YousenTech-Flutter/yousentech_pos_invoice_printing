9c871a080400873c2501c3cc176c9646f351aa19
eemzain <hudazain55@gmail.com>
2025-08-12 15:43:00 +0300

    .

--- lib/print_invoice/utils/test.dart
+++ lib/print_invoice/utils/test.dart
@@ -5,6 +5,7 @@ import 'dart:ui';
 
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
+import 'package:flutter_bluetooth_printer/flutter_bluetooth_printer_library.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
 import 'package:pos_shared_preferences/pos_shared_preferences.dart';
@@ -16,8 +17,7 @@ class ScreenshotWidget extends StatefulWidget {
   final Widget child;
   final String? printerIp;
   // ignore: prefer_const_constructors_in_immutables
-  ScreenshotWidget(
-      {required this.child, required this.printerIp, Key? key})
+  ScreenshotWidget({required this.child, required this.printerIp, Key? key})
       : super(key: key);
 
   @override
@@ -25,25 +25,25 @@ class ScreenshotWidget extends StatefulWidget {
 }
 
 class _ScreenshotWidgetState extends State<ScreenshotWidget> {
-
   ScreenshotController screenshotController = ScreenshotController();
+  ReceiptController? controller;
   @override
   void initState() {
     super.initState();
     print("===============initState");
     WidgetsBinding.instance.addPostFrameCallback((_) async {
       print("WidgetsBinding.instance.addPostFrameCallback ===============");
-      screenshotController.capture(
+      screenshotController
+          .capture(
         delay: const Duration(milliseconds: 200),
-      ).then((image) async {
+      )
+      .then((image) async {
         print("=========image ${image!.length}");
-        testPrint(imageThatC: image ,printerIp:widget.printerIp );
-        
-      }).whenComplete((){
+        testPrint(imageThatC: image, printerIp: widget.printerIp);
+      }).whenComplete(() {
         print("=========whenComplete");
         Get.back();
       });
-      
     });
   }
 
@@ -52,9 +52,16 @@ class _ScreenshotWidgetState extends State<ScreenshotWidget> {
     print("===============Widget build");
     return Scaffold(
       backgroundColor: AppColor.white,
-      body: Screenshot(
-        controller:screenshotController,
-        child: SizedBox(width: 150.w, child: widget.child),
+      body: Receipt(
+        builder: (context) => Screenshot(
+          controller: screenshotController,
+          child: SizedBox(
+              width: 150.w, child: widget.child),
+        ),
+        onInitialized: (controller) {
+          controller.paperSize = PaperSize.mm80;
+          this.controller = controller;
+        },
       ),
     );
   }
